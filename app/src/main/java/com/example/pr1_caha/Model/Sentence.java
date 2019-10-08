f816c8def9f5d37ff4706ec7facb41636250ac5a
Melissa Cejas <melissacejas@live.nl>
2019-10-08 16:17:50 +0200

    Added info from deleted sentence to other sentence file

--- app/src/main/java/com/example/pr1_caha/Model/Sentence.java
+++ app/src/main/java/com/example/pr1_caha/Model/Sentence.java
@@ -1,4 +1,26 @@
-package com.example.pr1_caha.Model;
+package com.example.pr1_caha;
+
+import java.util.ArrayList;
+
 
 public class Sentence {
+    private String sentence;
+    private static ArrayList<Sentence> SentencesList;
+
+    public Sentence(String sentence, ArrayList<Sentence> sentencesList) {
+        this.sentence = sentence;
+        sentencesList = new ArrayList<Sentence>();
+    }
+
+    private void updateSentence(){
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public getGallery() {
+        return getGallery();
+    }
+
 }
