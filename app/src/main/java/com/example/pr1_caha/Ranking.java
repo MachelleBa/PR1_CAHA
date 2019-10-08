2e2823a5128ed2e97df9f6d8e39526260f327ded
Jordy <jordyvoorham@hotmail.com>
2019-10-08 15:35:47 +0200

    Ranking class

---
+++ app/src/main/java/com/example/pr1_caha/Ranking.java
@@ -0,0 +1,17 @@
+package com.example.pr1_caha;
+
+import java.util.ArrayList;
+
+public class Ranking {
+
+    private int rankingValue;
+    private ArrayList<Photo> photo;
+
+    public void displayRanking(){
+
+    }
+
+    public void orderRanking(){
+
+    }
+}
