604f15f257470099ae1c65b45762786b763ac410
Melissa Cejas <melissacejas@live.nl>
2019-10-08 15:42:00 +0200

    Added sentence class
    Skeleton defined

---
+++ app/src/main/java/com/example/pr1_caha/Sentence.java
@@ -0,0 +1,22 @@
+package com.example.pr1_caha;
+
+import java.util.ArrayList;
+
+
+public class Sentence {
+    private String sentence;
+    private static ArrayList<Sentence> sentenceArrayList( )
+
+
+    private void updateSentence(){
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public String getGallery() {
+        return sentence;
+    }
+
+}
