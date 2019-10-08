317f8261edb7c62f008cc4f7e7e68a57748f22d1
shaquille s <shaquille.s@live.nl>
2019-10-08 15:21:56 +0200

    Photo class
    photo class toegevoegd

---
+++ app/src/main/java/com/example/pr1_caha/Photo.java
@@ -0,0 +1,13 @@
+package com.example.pr1_caha;
+
+public class Photo {
+    private String photoName;
+
+    public void getPhoto(){
+
+    }
+
+    public int getPhotoRanking(){
+        return 0;
+    }
+}
