76479d4f78a9648da3be5ba1877508006b164b41
Machelle <613954@student.inholland.nl>
2019-10-08 17:02:09 +0200

    Added Page class and Updated User class

--- app/src/main/java/com/example/pr1_caha/Model/User.java
+++ app/src/main/java/com/example/pr1_caha/Model/User.java
@@ -3,6 +3,8 @@ package com.example.pr1_caha.Model;
 //let op, Gallery en Image zijn bestaande android classes!
 //TODO: add imports
 
+import android.widget.Gallery;
+
 public class User {
 
     private int userId;
@@ -10,28 +12,38 @@ public class User {
     private String userTag;
     private Photo userIcon;
 
+    private Gallery gallery;
+
+    //TODO: user doesn't need to know which session they have, change it in the CD!
+
     public User(int id) {
 
         this.userId = id;
+
+       //TODO: make this gallery generation work!
+
+       // this.gallery = new Gallery();
     }
 
     //Deze constructer wordt gebruikt op het moment dat een user wordt aangemaakt, of geen icon heeft gekozen
-    public User(int id, String name, String tag) {
+    public User(int id, String name, String tag, Gallery gallery) {
 
         this.userId = id;
         this.userName = name;
         this.userTag = tag;
+        this.gallery = gallery;
 
         //TODO: voeg hier een standaard icon aan toe, dmv een methode!
       //  this.userIcon = standaardIcon
     }
 
     //Deze constructor hebben we nodig op het moment dat de user vanuit het JSON file een icon heeft
-    public User(int id, String name, String tag, Photo icon){
+    public User(int id, String name, String tag, Photo icon, Gallery gallery){
 
         this.userId = id;
         this.userName = name;
         this.userTag = tag;
+        this.gallery = gallery;
         this.userIcon = icon;
     }
 
@@ -53,5 +65,11 @@ public class User {
 
 
         //this.userIcon = image;
+    }
+
+    public void loginUser() {
+
+        //vraag hier de gegevens en vergelijk ze met de users in het JSON bestand, log dan in en maak een session aan
+
     }
 }
